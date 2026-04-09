1ec488cafc22318a356de8d562e36620a7f10239
MuhammadAhmad <muhammad.ahmad@uworx.co.uk>
2026-04-09 15:19:24 +0500

    adding types for files

--- src/main/java/com/github/baeyung/poslang/phtmlang/PhtmLanguage.java
+++ src/main/java/com/github/baeyung/poslang/phtmlang/PhtmLanguage.java
@@ -1,12 +1,13 @@
 package com.github.baeyung.poslang.phtmlang;
 
 import com.intellij.lang.Language;
+import com.intellij.lang.html.HTMLLanguage;
 
 public class PhtmLanguage extends Language {
 
     public static final PhtmLanguage INSTANCE = new PhtmLanguage();
 
     private PhtmLanguage() {
-        super("PhtmLang");
+        super(HTMLLanguage.INSTANCE, "PhtmLang");
     }
 }
--- src/main/java/com/github/baeyung/poslang/statelang/StateLanguage.java
+++ src/main/java/com/github/baeyung/poslang/statelang/StateLanguage.java
@@ -1,12 +1,13 @@
 package com.github.baeyung.poslang.statelang;
 
 import com.intellij.lang.Language;
+import com.intellij.lang.xml.XMLLanguage;
 
 public class StateLanguage extends Language {
 
     public static final StateLanguage INSTANCE = new StateLanguage();
 
     private StateLanguage() {
-        super("StateLang");
+        super(XMLLanguage.INSTANCE, "StateLang");
     }
 }
