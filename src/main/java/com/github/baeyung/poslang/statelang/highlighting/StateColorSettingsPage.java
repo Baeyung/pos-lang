53acbac5757fcc900bb693f6797aa675d84dd12d
MuhammadAhmad <muhammad.ahmad@uworx.co.uk>
2026-04-14 15:52:39 +0500

    fixing lexer + updating example in color settings, updating deps version
    # Conflicts:
    #	src/main/java/com/github/baeyung/poslang/statelang/state.flex

--- src/main/java/com/github/baeyung/poslang/statelang/highlighting/StateColorSettingsPage.java
+++ src/main/java/com/github/baeyung/poslang/statelang/highlighting/StateColorSettingsPage.java
@@ -16,9 +16,9 @@ public class StateColorSettingsPage implements ColorSettingsPage {
 
     private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[]{
             new AttributesDescriptor("Brackets", StateSyntaxHighlighter.BRACKETS),
-            new AttributesDescriptor("Tag Name", StateSyntaxHighlighter.TAG_NAME),
-            new AttributesDescriptor("Attribute Name", StateSyntaxHighlighter.IDENTIFIER),
-            new AttributesDescriptor("Attribute Value", StateSyntaxHighlighter.STRING),
+            new AttributesDescriptor("Tag name", StateSyntaxHighlighter.TAG_NAME),
+            new AttributesDescriptor("Attribute name", StateSyntaxHighlighter.IDENTIFIER),
+            new AttributesDescriptor("Attribute value", StateSyntaxHighlighter.STRING),
             new AttributesDescriptor("Comment", StateSyntaxHighlighter.COMMENT),
             new AttributesDescriptor("Operator", StateSyntaxHighlighter.OPERATOR),
     };
@@ -38,10 +38,67 @@ public class StateColorSettingsPage implements ColorSettingsPage {
     @NotNull
     @Override
     public String getDemoText() {
-        return "<!-- Example of State File -->\n" +
-                "<Button class=\"primary\" disabled=\"false\">\n" +
-                "    <Text value=\"Click Me\" />\n" +
-                "</Button>\n";
+        return """
+                <!-- Root level comment -->
+                <statefile loader="mainLoader.phtm">
+                    <!-- Include variations -->
+                    <include file="common.states" exclude="debug"/>
+
+                    <!-- Exit definition -->
+                    <exit name="endState" mainState="true" rootStart="yes"/>
+
+                    <!-- Global event (allowed in statefileBody) -->
+                    <event name="globalEvent" next="initState" audit="true" pnp="yes"/>
+
+                    <!-- Global data -->
+                    <data name="globalVar" value="123" calculate="false" comment="global data"/>
+
+                    <!-- First state -->
+                    <state name="initState" frame="mainFrame" helpRef="help1" like="base"
+                           sound="start.wav" page="home" prompt="Welcome"
+                           picture="img.png" keyboard="default" comment="initial state">
+
+                        <!-- State level comment -->
+                        <!-- This is inside state -->
+
+                        <!-- State data -->
+                        <data name="counter" value="0" calculate="true" comment="counter data"/>
+
+                        <!-- State event -->
+                        <event name="onStart"
+                               next="processingState"
+                               callSubstate="subFlow"
+                               substateNext="resumeHere"
+                               permission="admin"
+                               permissionFail="deniedState"
+                               ppi="ppiValue"
+                               audit="true"
+                               pnp="yes"
+                               comment="event comment"/>
+
+                        <!-- Include inside state -->
+                        <include file="nested.states"/>
+
+                    </state>
+
+                    <!-- Second state -->
+                    <state name="processingState" frame="procFrame" page="processPage">
+
+                        <data name="temp" value="42"/>
+
+                        <event name="onProcess" Next="endState"/>
+
+                    </state>
+
+                    <!-- Third state with minimal attrs -->
+                    <state name="endState">
+
+                        <event name="finish"/>
+
+                    </state>
+
+                </statefile>
+        """;
     }
 
     @Nullable
