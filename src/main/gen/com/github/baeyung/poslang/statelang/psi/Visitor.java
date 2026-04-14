266269408d7de0cf442a5270a8c1cb5ea85c6ea7
Muhammad Ahmad <ahmadsheikh617@gmail.com>
2026-04-15 01:37:36 +0500

    regenerating files from .flex and .bnf

---
+++ src/main/gen/com/github/baeyung/poslang/statelang/psi/Visitor.java
@@ -0,0 +1,46 @@
+// This is a generated file. Not intended for manual editing.
+package com.github.baeyung.poslang.statelang.psi;
+
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiElement;
+
+public class Visitor extends PsiElementVisitor {
+
+  public void visitAttribute(@NotNull Attribute o) {
+    visitPsiElement(o);
+  }
+
+  public void visitCommentTag(@NotNull CommentTag o) {
+    visitPsiElement(o);
+  }
+
+  public void visitContent(@NotNull Content o) {
+    visitPsiElement(o);
+  }
+
+  public void visitElement(@NotNull Element o) {
+    visitPsiElement(o);
+  }
+
+  public void visitEndTag(@NotNull EndTag o) {
+    visitPsiElement(o);
+  }
+
+  public void visitSelfClosingTag(@NotNull SelfClosingTag o) {
+    visitPsiElement(o);
+  }
+
+  public void visitStartTag(@NotNull StartTag o) {
+    visitPsiElement(o);
+  }
+
+  public void visitTag(@NotNull Tag o) {
+    visitPsiElement(o);
+  }
+
+  public void visitPsiElement(@NotNull PsiElement o) {
+    visitElement(o);
+  }
+
+}
