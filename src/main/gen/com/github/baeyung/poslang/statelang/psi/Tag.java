c446ae63036c80616128952216f88eab11ead27c
Muhammad Ahmad <ahmadsheikh617@gmail.com>
2026-04-15 00:54:14 +0500

    regenerating sources

---
+++ src/main/gen/com/github/baeyung/poslang/statelang/psi/Tag.java
@@ -0,0 +1,22 @@
+// This is a generated file. Not intended for manual editing.
+package com.github.baeyung.poslang.statelang.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface Tag extends PsiElement {
+
+  @NotNull
+  List<Content> getContentList();
+
+  @Nullable
+  EndTag getEndTag();
+
+  @Nullable
+  SelfClosingTag getSelfClosingTag();
+
+  @Nullable
+  StartTag getStartTag();
+
+}
