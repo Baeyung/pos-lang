565702896128a1309a758934aab7f7fcd4069267
Muhammad Ahmad <ahmadsheikh617@gmail.com>
2026-04-22 00:50:11 +0500

    updating the grammar + util to retrieve main sets

--- src/main/java/com/github/baeyung/poslang/statelang/utils/psi/StatePsiImplUtil.java
+++ src/main/java/com/github/baeyung/poslang/statelang/utils/psi/StatePsiImplUtil.java
@@ -1,92 +1,116 @@
-//package com.github.baeyung.poslang.statelang.utils.psi;
-//
-//import com.github.baeyung.poslang.statelang.psi.Attribute;
-//import com.github.baeyung.poslang.statelang.psi.EndTag;
-//import com.github.baeyung.poslang.statelang.psi.SelfClosingTag;
-//import com.github.baeyung.poslang.statelang.psi.StartTag;
-//import com.github.baeyung.poslang.statelang.psi.StateTypes;
-//import com.intellij.lang.ASTNode;
-//import com.intellij.openapi.util.text.StringUtil;
-//
-//public class StatePsiImplUtil
-//{
-//    public static String getKey(Attribute element)
-//    {
-//        ASTNode keyNode = element
-//                .getNode()
-//                .findChildByType(StateTypes.IDENTIFIER);
-//
-//        if (keyNode != null && StringUtil.isNotEmpty(keyNode.getText()))
-//        {
-//            return keyNode.getText().toLowerCase();
-//        }
-//        else
-//        {
-//            return null;
-//        }
-//    }
-//
-//    public static String getValue(Attribute element)
-//    {
-//        ASTNode valueNode = element
-//                .getNode()
-//                .findChildByType(StateTypes.STRING);
-//
-//        if (valueNode != null && StringUtil.isNotEmpty(valueNode.getText()))
-//        {
-//            return valueNode.getText().toLowerCase();
-//        }
-//        else
-//        {
-//            return null;
-//        }
-//    }
-//
-//    public static String getTagName(StartTag element)
-//    {
-//        ASTNode nameNode = element
-//                .getNode()
-//                .findChildByType(StateTypes.TAG_NAME);
-//
-//        if (nameNode != null && StringUtil.isNotEmpty(nameNode.getText()))
-//        {
-//            return nameNode.getText().toLowerCase();
-//        }
-//        else
-//        {
-//            return null;
-//        }
-//    }
-//
-//    public static String getTagName(EndTag element)
-//    {
-//        ASTNode nameNode = element
-//                .getNode()
-//                .findChildByType(StateTypes.TAG_NAME);
-//
-//        if (nameNode != null && StringUtil.isNotEmpty(nameNode.getText()))
-//        {
-//            return nameNode.getText().toLowerCase();
-//        }
-//        else
-//        {
-//            return null;
-//        }
-//    }
-//
-//    public static String getTagName(SelfClosingTag element)
-//    {
-//        ASTNode nameNode = element
-//                .getNode()
-//                .findChildByType(StateTypes.TAG_NAME);
-//
-//        if (nameNode != null && StringUtil.isNotEmpty(nameNode.getText()))
-//        {
-//            return nameNode.getText().toLowerCase();
-//        }
-//        else
-//        {
-//            return null;
-//        }
-//    }
-//}
+package com.github.baeyung.poslang.statelang.utils.psi;
+
+import com.github.baeyung.poslang.statelang.psi.Attribute;
+import com.github.baeyung.poslang.statelang.psi.EndTag;
+import com.github.baeyung.poslang.statelang.psi.PairedTag;
+import com.github.baeyung.poslang.statelang.psi.SelfClosingTag;
+import com.github.baeyung.poslang.statelang.psi.StartTag;
+import com.github.baeyung.poslang.statelang.psi.StateTypes;
+import com.github.baeyung.poslang.statelang.psi.Tag;
+import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.text.StringUtil;
+
+public class StatePsiImplUtil
+{
+    public static String getKey(Attribute element)
+    {
+        ASTNode keyNode = element
+                .getNode()
+                .findChildByType(StateTypes.IDENTIFIER);
+
+        if (keyNode != null && StringUtil.isNotEmpty(keyNode.getText()))
+        {
+            return keyNode
+                    .getText()
+                    .toLowerCase();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static String getValue(Attribute element)
+    {
+        ASTNode valueNode = element
+                .getNode()
+                .findChildByType(StateTypes.STRING);
+
+        if (valueNode != null && StringUtil.isNotEmpty(valueNode.getText()))
+        {
+            return valueNode
+                    .getText()
+                    .toLowerCase();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static String getTagName(StartTag element)
+    {
+        ASTNode nameNode = element
+                .getNode()
+                .findChildByType(StateTypes.TAG_NAME);
+
+        if (nameNode != null && StringUtil.isNotEmpty(nameNode.getText()))
+        {
+            return nameNode
+                    .getText()
+                    .toLowerCase();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static String getTagName(Tag tag) {
+        if (tag instanceof PairedTag pairedTag) {
+            return getTagName(pairedTag.getStartTag());
+        }
+
+        if (tag instanceof SelfClosingTag selfClosingTag) {
+            return getTagName(selfClosingTag);
+        }
+
+        return null;
+    }
+
+    public static String getTagName(EndTag element)
+    {
+        ASTNode nameNode = element
+                .getNode()
+                .findChildByType(StateTypes.TAG_NAME);
+
+        if (nameNode != null && StringUtil.isNotEmpty(nameNode.getText()))
+        {
+            return nameNode
+                    .getText()
+                    .toLowerCase();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static String getTagName(SelfClosingTag element)
+    {
+        ASTNode nameNode = element
+                .getNode()
+                .findChildByType(StateTypes.TAG_NAME);
+
+        if (nameNode != null && StringUtil.isNotEmpty(nameNode.getText()))
+        {
+            return nameNode
+                    .getText()
+                    .toLowerCase();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
