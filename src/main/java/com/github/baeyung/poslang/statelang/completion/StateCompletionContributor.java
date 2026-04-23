a3214e75c5d4cf2c4618953b6c9a8d0e8708544f
Muhammad Ahmad <ahmadsheikh617@gmail.com>
2026-04-24 00:49:55 +0500

    adding auto completion

--- src/main/java/com/github/baeyung/poslang/statelang/completion/StateCompletionContributor.java
+++ src/main/java/com/github/baeyung/poslang/statelang/completion/StateCompletionContributor.java
@@ -3,7 +3,10 @@ package com.github.baeyung.poslang.statelang.completion;
 import com.github.baeyung.poslang.statelang.psi.StateTypes;
 import com.github.baeyung.poslang.statelang.spec.StateLanguageSpec;
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
@@ -19,7 +22,8 @@ public class StateCompletionContributor extends CompletionContributor
     public StateCompletionContributor()
     {
         extend(
-                CompletionType.BASIC, PlatformPatterns.psiElement(StateTypes.TAG_NAME),
+                CompletionType.BASIC,
+                PlatformPatterns.psiElement(StateTypes.TAG_NAME),
                 new CompletionProvider<>()
                 {
                     @Override
@@ -31,19 +35,34 @@ public class StateCompletionContributor extends CompletionContributor
                     {
                         for (String tag : StateLanguageSpec.ALLOWED_FULL_TAGS)
                         {
-                            result.addElement(LookupElementBuilder.create(tag));
+                            result.addElement(
+                                    LookupElementBuilder
+                                            .create(tag)
+                                            .withInsertHandler(
+                                                    (ctx, item) ->
+                                                            handleTagInsert(ctx, item.getLookupString(), false)
+                                            )
+                            );
                         }
 
                         for (String tag : StateLanguageSpec.ALLOWED_SELF_CLOSING_TAGS)
                         {
-                            result.addElement(LookupElementBuilder.create(tag));
+                            result.addElement(
+                                    LookupElementBuilder
+                                            .create(tag)
+                                            .withInsertHandler(
+                                                    (ctx, item) ->
+                                                            handleTagInsert(ctx, item.getLookupString(), true)
+                                            )
+                            );
                         }
                     }
                 }
         );
 
         extend(
-                CompletionType.BASIC, PlatformPatterns.psiElement(StateTypes.IDENTIFIER),
+                CompletionType.BASIC,
+                PlatformPatterns.psiElement(StateTypes.IDENTIFIER),
                 new CompletionProvider<>()
                 {
                     @Override
@@ -59,32 +78,79 @@ public class StateCompletionContributor extends CompletionContributor
 
                         if (tagName != null && StateLanguageSpec.TAG_ATTRIBUTES.containsKey(tagName))
                         {
-                            for (String attr : StateLanguageSpec.TAG_ATTRIBUTES.get(tagName).getAttributes())
+                            for (String attr : StateLanguageSpec.TAG_ATTRIBUTES
+                                    .get(tagName)
+                                    .getAttributes())
                             {
-                                result.addElement(LookupElementBuilder.create(attr));
+                                result.addElement(
+                                        LookupElementBuilder
+                                                .create(attr)
+                                                .withInsertHandler(StateCompletionContributor::handleAttributeInsert)
+                                );
                             }
                         }
                     }
-
-                    private static @Nullable String getTagName(PsiElement position)
-                    {
-                        StartTag startTag = PsiTreeUtil.getParentOfType(position, StartTag.class);
-                        SelfClosingTag selfClosingTag = PsiTreeUtil.getParentOfType(position, SelfClosingTag.class);
-
-                        String tagName = null;
-                        if (startTag != null)
-                        {
-                            TagNameEl tagNameEl = startTag.getTagNameEl();
-                            tagName = tagNameEl.getText();
-                        }
-                        else if (selfClosingTag != null)
-                        {
-                            TagNameEl tagNameEl = selfClosingTag.getTagNameEl();
-                            tagName = tagNameEl.getText();
-                        }
-                        return tagName;
-                    }
                 }
         );
+    }
+
+    private static @Nullable String getTagName(PsiElement position)
+    {
+        StartTag startTag = PsiTreeUtil.getParentOfType(position, StartTag.class);
+        SelfClosingTag selfClosingTag = PsiTreeUtil.getParentOfType(position, SelfClosingTag.class);
+
+        String tagName = null;
+        if (startTag != null)
+        {
+            TagNameEl tagNameEl = startTag.getTagNameEl();
+            tagName = tagNameEl.getText();
+        }
+        else if (selfClosingTag != null)
+        {
+            TagNameEl tagNameEl = selfClosingTag.getTagNameEl();
+            tagName = tagNameEl.getText();
+        }
+        return tagName;
+    }
+
+    private static void handleAttributeInsert(InsertionContext context, LookupElement item)
+    {
+        Document document = context.getDocument();
+        Editor editor = context.getEditor();
+
+        int tailOffset = context.getTailOffset();
+        CharSequence text = document.getCharsSequence();
+
+        if (tailOffset < text.length() && text.charAt(tailOffset) == '=')
+        {
+            return;
+        }
+
+        document.insertString(tailOffset, "=\"\"");
+        editor.getCaretModel().moveToOffset(tailOffset + 2);
+    }
+
+    private static void handleTagInsert(InsertionContext context, String tagName, boolean selfClosing)
+    {
+        Document document = context.getDocument();
+        Editor editor = context.getEditor();
+
+        int tailOffset = context.getTailOffset();
+        CharSequence text = document.getCharsSequence();
+
+        if (tailOffset < text.length() && text.charAt(tailOffset) == '>')
+        {
+            return;
+        }
+
+        if (selfClosing)
+        {
+            document.insertString(tailOffset, "  />");
+        }
+        else
+        {
+            document.insertString(tailOffset, " ></" + tagName + ">");
+        }
+        editor.getCaretModel().moveToOffset(tailOffset + 1);
     }
 }
